4fbd3db33d6b6a81afc1761bafed1ebf080f6144
gildevson <gildevson@gmail.com>
2025-11-09 01:41:07 -0300

    Adicionando mais dados ao meus usuarios estrutural

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,9 +2,17 @@
 using Dapper;
 using FinanblueBackend.Data;    
 using FinanblueBackend.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FinanblueBackend.Controllers {
     public class UserController {
+        [ApiController]
+        [Route("api/controller")]
+
+        public class UsersController : ControllerBase { 
+        
+        
+        }
 
     }
 }
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; } 
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string SenhaHash { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
         public DateTime DataCriacao { get; set; } = DateTime.Now;
     }
 }
